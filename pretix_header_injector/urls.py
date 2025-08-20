3170b624356f7b227c51dd2dbecc03614b83b7c7
jxs714 <jxproductions@gmx.com>
2025-08-20 07:30:01 -0500

    Update urls.py

--- pretix_header_injector/urls.py
+++ pretix_header_injector/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
-from .views import SettingsView
 
 urlpatterns = [
-    path(
-        'control/event/<str:organizer>/<str:event>/settings/headerinjector/',
-        SettingsView.as_view(),
-        name='settings'
-    ),
 ]
