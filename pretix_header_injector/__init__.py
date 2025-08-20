946646cf95965063ca844c2ea16209bda2c2f484
jxs714 <jxproductions@gmx.com>
2025-08-20 03:51:31 -0500

    Update __init__.py

--- pretix_header_injector/__init__.py
+++ pretix_header_injector/__init__.py
@@ -1,16 +1,16 @@
-from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
+from pretix.base.plugins import PluginConfig # <- This line is changed
 
 class PretixPluginMeta:
     name = _('Header Code Injector')
     author = _('Your Name')
-    version = '1.0.1'
+    version = '1.0.2' # I've bumped the version number
     description = _('This plugin allows you to inject custom code (e.g., Google Analytics, Meta Pixel) into the HTML head of your event pages.')
     category = 'CUSTOMIZATION'
     visible = True
     featured = False
 
-class HeaderInjectorApp(AppConfig):
+class HeaderInjectorApp(PluginConfig): # <- This line is changed
     name = 'pretix_header_injector'
     verbose_name = 'Header Code Injector'
 
