f00f5714b3c0322cd9f903f06dc53fe9d5043872
jxs714 <jxproductions@gmx.com>
2025-08-20 05:07:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='pretix-header-injector',
     version='1.0.0',
-    description='Embed any code into Pretix header event pages',
-    author='Your Name',
-    author_email='your.email@example.com',
-    license='Apache Software License',
-    
+    description='Embed code into Pretix headers',
     packages=find_packages(),
     include_package_data=True,
-    zip_safe=False,
-    
-    install_requires=[],
-    
     entry_points="""
 [pretix.plugin]
 pretix_header_injector=pretix_header_injector:PretixPluginMeta
