d5bb559a16d86f549e6d9c2866fe322c5f7dd009
jxs714 <jxproductions@gmx.com>
2025-08-20 04:54:53 -0500

    Update __init__.py

--- pretix_header_injector/__init__.py
+++ pretix_header_injector/__init__.py
@@ -1,24 +1,10 @@
-from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
+class PretixPluginMeta:
+    name = _('Header Injector')
+    author = 'Your Name'
+    description = _('Embed any code into Pretix header event pages')
+    visible = True
+    version = '1.0.0'
 
-class PluginApp(AppConfig):
-    name = 'pretix_header_injector'
-    verbose_name = 'Pretix Header Injector'
-
-    class PretixPluginMeta:
-        name = _('Header Injector')
-        author = 'Your Name'
-        description = _('Embed any code into Pretix header event pages')
-        visible = True
-        version = '1.0.0'
-        category = 'CUSTOMIZATION'
-        compatibility = "pretix>=3.0.0"
-
-    def ready(self):
-        from . import signals  # noqa
-
-
-# Make PretixPluginMeta available at module level for entry point
-PretixPluginMeta = PluginApp.PretixPluginMeta
 default_app_config = 'pretix_header_injector.PluginApp'
