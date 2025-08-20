7006d630d76b5963b0d802a6b91d092c07126849
jxs714 <jxproductions@gmx.com>
2025-08-20 07:40:04 -0500

    Update __init__.py

--- pretix_header_injector/__init__.py
+++ pretix_header_injector/__init__.py
@@ -1,2 +1,4 @@
-# The following line is the only thing this file should contain
+__version__ = "1.0.0"
+
+# The following line is the only other thing this file should contain
 default_app_config = "pretix_header_injector.apps.PluginApp"
