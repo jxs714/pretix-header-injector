1b3ff7b97d118524d43506dbf2cf64057861ec3c
jxs714 <jxproductions@gmx.com>
2025-08-20 04:50:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,45 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
 from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
 
 setup(
     name='pretix-header-injector',
     version='1.0.0',
-    description='A pretix plugin to inject custom code into the HTML head.',
+    description='Embed any code into Pretix header event pages',
+    long_description=long_description,
+    url='https://github.com/jxs714/pretix-header-injector',
     author='Your Name',
     author_email='your.email@example.com',
+    license='Apache',
+
+    install_requires=[],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
+    cmdclass=cmdclass,
+
     entry_points="""
 [pretix.plugin]
-pretix_header_injector=pretix_header_injector:HeaderInjectorApp
-"""
+pretix_header_injector=pretix_header_injector:PretixPluginMeta
+""",
 )
