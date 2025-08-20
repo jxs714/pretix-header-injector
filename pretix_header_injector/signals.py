2e2541895e6f8b9542975032f6f0ad7bd8d0b815
jxs714 <jxproductions@gmx.com>
2025-08-20 04:57:12 -0500

    Update signals.py

--- pretix_header_injector/signals.py
+++ pretix_header_injector/signals.py
@@ -1,13 +1,10 @@
 from django.dispatch import receiver
-from django.utils.safestring import mark_safe
 from pretix.presale.signals import html_head
-from pretix.base.settings import settings_hierarkey
-
-settings_hierarkey.add_default('header_injector_code', '', str)
 
 @receiver(html_head, dispatch_uid="header_injector_html_head")
-def header_injector_html_head(sender, request, **kwargs):
-    code = sender.settings.get('header_injector_code')
-    if code:
-        return mark_safe(code)
-    return ""
+def add_header_code(sender, request, **kwargs):
+    """
+    Add custom header code to event pages
+    """
+    # Your header injection logic here
+    return ""  # Return your custom HTML/JS code
