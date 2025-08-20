8e183f4c9d0f204653d52f7cfc67e80d0522719e
jxs714 <jxproductions@gmx.com>
2025-08-20 04:08:37 -0500

    Update __init__.py

--- pretix_header_injector/__init__.py
+++ pretix_header_injector/__init__.py
@@ -1,15 +1,15 @@
+from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
-from pretix.base.plugins import PluginConfig
 
-class HeaderInjectorApp(PluginConfig):
+class HeaderInjectorApp(AppConfig):
     name = 'pretix_header_injector'
     verbose_name = 'Header Code Injector'
 
     class PretixPluginMeta:
         name = _('Header Code Injector')
         author = _('Your Name')
-        version = '2.0.0' # Bumping version to guarantee a fresh install
-        description = _('Injects custom code (e.g., Google Analytics, Meta Pixel) into the HTML head of event pages.')
+        version = '3.0.0' # Final version
+        description = _('Injects custom code into the HTML head of event pages.')
         visible = True
         category = 'CUSTOMIZATION'
         featured = False
