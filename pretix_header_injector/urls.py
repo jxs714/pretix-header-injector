453fbd5bfc2c9f0084003f709dea1d6b76888c06
jxs714 <jxproductions@gmx.com>
2025-08-20 05:00:53 -0500

    Update urls.py

--- pretix_header_injector/urls.py
+++ pretix_header_injector/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+# This defines the URL patterns for your plugin
+# The 'app_name' is important for namespacing
+app_name = 'pretix_header_injector'
+
 urlpatterns = [
-    # Your URL patterns here
+    # Settings page URL
+    # This will be accessible at: /control/event/{organizer}/{event}/settings/plugins/header-injector/
+    path('settings/', views.HeaderInjectorSettingsView.as_view(), name='settings'),
 ]
