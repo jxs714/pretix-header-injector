4a7b97b8304b74dfbd2b4de95760be82014e1b42
jxs714 <jxproductions@gmx.com>
2025-08-20 04:25:46 -0500

    Update __init__.py

--- pretix_header_injector/__init__.py
+++ pretix_header_injector/__init__.py
@@ -1,20 +1,19 @@
-from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
+from pretix.base.plugins import PluginConfig
 
-class HeaderInjectorApp(AppConfig):
+class HeaderInjectorApp(PluginConfig):
     name = 'pretix_header_injector'
     verbose_name = 'Header Code Injector'
 
-    class PretixPluginMeta:
-        name = _('Header Code Injector')
-        author = _('Your Name')
-        version = '5.0.0'
-        description = _('Injects custom code into the HTML head of event pages.')
-        visible = True
-        category = 'CUSTOMIZATION'
-        featured = False
+    # All metadata is now directly part of this class
+    version = '6.0.0'
+    author = 'Your Name'
+    description = _('Injects custom code into the HTML head of event pages.')
+    category = 'CUSTOMIZATION'
+    visible = True
+    featured = False
 
     def ready(self):
-        from . import signals  # NOQA
+        from . import signals
 
 default_app_config = 'pretix_header_injector.HeaderInjectorApp'
