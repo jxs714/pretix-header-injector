e3705e203c5c08ece6e68cd2246e6d9b6ef7ddde
jxs714 <jxproductions@gmx.com>
2025-08-20 05:59:03 -0500

    Delete pretix_header_injector directory

--- pretix_header_injector/signals.py
+++
@@ -1,7 +0,0 @@
-from django.dispatch import receiver
-from django.utils.safestring import mark_safe
-from pretix.presale.signals import html_head
-
-@receiver(html_head, dispatch_uid="header_injector")
-def inject_header(sender, request, **kwargs):
-    return mark_safe('<!-- Header Injector Active -->')
