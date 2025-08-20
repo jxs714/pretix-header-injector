57e5b2580c36c920a68c577f75c0bd6339ed4627
jxs714 <jxproductions@gmx.com>
2025-08-20 07:29:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-setup()
+setup(
+    name='pretix-header-injector',
+    version='1.0.0',
+    description='A pretix plugin to inject custom code into the HTML head.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    entry_points="""
+[pretix.plugin]
+pretix_header_injector=pretix_header_injector:HeaderInjectorApp
+"""
+)
